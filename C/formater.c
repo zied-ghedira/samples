f9e8ee3ed884d702b01a3132eaad3d7acfe6feb3
zied-ghedira <zied.ghedira@gmail.com>
2022-02-11 20:26:05 +0100

    Update formater.c

--- C/formater.c
+++ C/formater.c
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
 
+int main() {
+    // Complete the code.
+    int i{0};
+    long l{0};
+    char c;
+    float f{0};
+    double d{0};
+    
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d \n",i);
+    printf("%ld \n",l);
+    printf("%c \n",c);
+    printf("%f \n",f);
+    printf("%lf \n",d);
+    return 0;
+}
