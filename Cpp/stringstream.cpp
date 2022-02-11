77477e15475178905fe5dde2e73fb7ed04d27047
zied-ghedira <zied.ghedira@gmail.com>
2022-02-11 20:29:34 +0100

    Update stringstream.cpp

--- Cpp/stringstream.cpp
+++ Cpp/stringstream.cpp
@@ -1,1 +1,26 @@
+/********************************************************************/
+/* Input is a string contaning 3 numbers seperated by space         */
+/********************************************************************/
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include<string>
+#include<sstream>
+
+using namespace std;
+
+int main() {
+    string s,word;
+    int a{0}, b{0}, c{0}, sum{0};
+    getline(cin, s);
+    stringstream ssin(s);
+
+    ssin >> a >> b >>c;
+    sum = a + b + c;
+
+    cout << sum;
+    
+    return 0;
+}
